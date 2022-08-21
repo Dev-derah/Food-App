6efd27850930e84c48c6e78760c3dc2581a1a606
Dev-derah <chidera261@gmail.com>
2022-08-21 15:13:01 +0100

    Added main section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
 import React from "react";
 import logo from "../images/logo.svg";
+import noodles from "../images/noodles.png";
+import appStoreBadge from "../images/AppStoreBadge.svg";
+import googleplayBadge from "../images/GooglePlayBadge.svg";
+import burger from "../images/burger.png";
+import spag from "../images/spag.png";
+import meatBalls from "../images/meat-balls.png";
 import { Link } from "react-router-dom";
 import "../css/Home.css";
 
 const Home = () => {
+  const special_meals = [
+    {
+      meal: "Stir fry Pasta",
+      meal_description: "Stir fry pasta yada yada yada because of Sesan",
+      meal_image: spag ,
+    },
+    {
+      meal_image: meatBalls ,
+      meal: "Meat Balls",
+      meal_description: "Stir fry pasta yada yada yada because of Sesan",
+    },
+    {
+      meal_image: burger ,
+      meal: "Burger Meal",
+      meal_description: "Stir fry pasta yada yada yada because of Sesan",
+    },
+  ];
   return (
     <div className="home-body">
       <nav className="navbar">
@@ -32,6 +55,82 @@ const Home = () => {
           </ul>
         </div>
       </nav>
+      {/* header */}
+      <header>
+        <div className="header-left">
+          <h2 className="jumbotron">Order food anytime, anywhere</h2>
+          <p className="jumbotron-subtext">
+            Browse from our list of specials to place your order and have food
+            delivered to you in no time. Affordable, tasty and fast!
+          </p>
+          <div className="download-btns">
+            <div className="cta-btn">
+              <img src={googleplayBadge} alt="google-play-badge" />
+            </div>
+            <div className="cta-btn">
+              <img src={appStoreBadge} alt="app-store-badge" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src={noodles} alt="noodles" className="header-img" />
+        </div>
+      </header>
+
+      {/* main */}
+      <main>
+        <div className="main-content">
+          <h1 className="main-header">Special Meals of the day!</h1>
+          <p className="main-subtext">
+            Check our sepecials of the day and get discounts on all our meals
+            and swift delivery to what ever location within Ilorin.
+          </p>
+          <div className="meals-container">
+            {special_meals.map((meal, index) => {
+              return (
+                <div className="special-meal">
+                  <img src={meal.meal_image} alt="food" />
+                  <h3>{meal.meal}</h3>
+                  <p>{meal.meal_description}</p>
+                </div>
+              );
+            })}
+            <div></div>
+          </div>
+        </div>
+
+        <div className="mailist-section">
+          <div className="mailist-left">
+            <h2 className="mailist-header">Get notified when we update!</h2>
+            <p className="mailist-subtext">
+              Get notified when we add new items to our specials menu, update
+              our price list of have promos!
+            </p>
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="gregphillips@gmail.com"
+              className="email"
+            ></input>
+            <button className="email-btn">Get notified</button>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div>
+          <p>Company</p>
+        </div>
+        <div>
+          <p>Company</p>
+        </div>
+        <div>
+          <p>Company</p>
+        </div>
+        <div>
+          <p>Company</p>
+        </div>
+      </footer>
     </div>
   );
 };
